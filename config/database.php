5d62112c21a1aeef8fa55dd7aa3fa1e12ab0589f
= <=>
2019-11-02 15:06:51 +0200

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$DATABASE_URL=parse_url('postgres://bxnvjmnekirrze:9349833466a8b7ee3300e0bfaad4dd9e518bdd8f309689a997857cd3a367ab07@ec2-174-129-253-104.compute-1.amazonaws.com:5432/d6os45kp3d393t');
+
 return [
 
     /*
@@ -13,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +58,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
